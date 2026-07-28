0b79f5d3e45908027b0423905b1f3951a43431c7
Snails <renish88@gmail.com>
2026-07-28 11:24:23 +0900

    콘솔 입력 진행

---
+++ ConsoleInput/Main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	int value = 0;
+
+	while (true)
+	{
+		char buffer[50] = {};
+		std::cout << "0이 아닌 숫자를 입력하세요(첫 번째):";
+		std::cin >> buffer;
+
+		if ((value = atoi(buffer)) == 0)
+		{
+			std::cout << "숫자를 입력하세요\n";
+		}
+		else
+			break;
+	}
+
+	int value2 = 0;
+	std::cout << "0이 아닌 숫자를 입력하세요(두 번째):";
+	std::cin >> value2;
+
+	std::cout << "입력한 두 수의 합은 " << (value + value2) << "입니다\n";
+
+	//std::cout << "입력하신 숫자는 " << value << "입니다\n";
+
+
+
+	std::cin.get();
+}
