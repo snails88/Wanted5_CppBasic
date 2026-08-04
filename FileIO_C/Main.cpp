3f053b439c9129b05bad292b236f5d35efa09cef
Snails <renish88@gmail.com>
2026-08-04 19:52:51 +0900

    파일입출력

---
+++ FileIO_C/Main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	FILE* file = nullptr;
+
+	fopen_s(&file, "Test.txt", "rb");	// 파일 열기
+
+	if (!file)
+	{
+		std::cout << "파일 열기 실패!\n";
+		return 1;
+	}
+
+	std::cout << "파일 열기 성공\n";
+
+	fclose(file);	// 파일 닫기
+	file = nullptr;
+}
--- Template/Main.cpp
+++ Template/Main.cpp
@@ -1,39 +1,39 @@
-﻿#include <iostream>
-
-template<typename T>
-void Swap(T& a, T& b)
-{
-	T temp = a;
-	a = b;
-	b = temp;
-}
-
-template<typename T, int size = 10>
-class Data
-{
-public:
-	Data(const T& newData)
-		:data(newData)
-private:
-	T data = {};
-
-	T GetData() const { return data; }
-	void SetData(const T& newData) { data = newData; }
-	int GetSize() { return size; }
-};
-
-
-int main()
-{
-	int a = 10;
-	int b = 30;
-
-	Swap<int>(a, b);
-
-	Data<int, 100> intData(30);
-	Data<float> pi(3.141592f);
-
-	std::swap<int>(a, b);
-
-	std::cin.get();
-}
+﻿//#include <iostream>
+//
+//template<typename T>
+//void Swap(T& a, T& b)
+//{
+//	T temp = a;
+//	a = b;
+//	b = temp;
+//}
+//
+//template<typename T, int size = 10>
+//class Data
+//{
+//public:
+//	Data(const T& newData)
+//		:data(newData)
+//private:
+//	T data = {};
+//
+//	T GetData() const { return data; }
+//	void SetData(const T& newData) { data = newData; }
+//	int GetSize() { return size; }
+//};
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 30;
+//
+//	Swap<int>(a, b);
+//
+//	Data<int, 100> intData(30);
+//	Data<float> pi(3.141592f);
+//
+//	std::swap<int>(a, b);
+//
+//	std::cin.get();
+////}
