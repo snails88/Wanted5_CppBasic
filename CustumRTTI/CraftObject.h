5d98eb3e570e1cc86100c3a9027148d5747c1277
Snails <renish88@gmail.com>
2026-08-04 12:05:49 +0900

    CustumRTTI

---
+++ CustumRTTI/CraftObject.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include <memory>
+
+// 커스텀 RTTI를 제공하는 기반 클래스
+class CraftObject
+{
+public:
+	virtual size_t GetType() const = 0;
+
+	// 여기까지 올라오면 다른 타입
+	virtual bool Is(size_t id) const
+	{
+		return false;
+	}
+
+	template<typename T>
+	bool IsTypeOf() const
+	{
+		return Is(T::TypeId());
+	}
+};
+
+// 스마트 포인터 형변환함수
+template<typename T, typename U>
+std::shared_ptr<T> Cast(const std::shared_ptr<U> object)
+{
+	if (!object)
+	{
+		return nullptr;
+	}
+
+	if (object->Is(T::TypeId()))
+	{
+		return std::static_pointer_cast<T>(object);
+	}
+
+	return nullptr;
+}
+
+#define TYPE_DECLARATIONS(Type, ParentType)							\
+	using super = ParentType;										\
+protected:															\
+	static size_t TypeIdClass()										\
+	{																\
+		static int runTimeTypeId = 0;								\
+		return reinterpret_cast<size_t>(&runTimeTypeId);			\
+	}																\
+public:																\
+	static size_t TypeId()											\
+	{																\
+		return Type::TypeIdClass();									\
+	}																\
+	virtual size_t GetType() const override							\
+	{																\
+		return Type::TypeIdClass();									\
+	}																\
+	virtual bool Is(size_t id) const override						\
+	{																\
+		return (id == TypeIdClass()) ? true : ParentType::Is(id);	\
+	}																
---
+++ CustumRTTI/Main.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include "CraftObject.h"
+
+class Actor : public CraftObject
+{
+	TYPE_DECLARATIONS(Actor, CraftObject)
+};
+
+class Player : public Actor
+{
+	TYPE_DECLARATIONS(Player, Actor)
+};
+
+class Camera : public Actor
+{
+	TYPE_DECLARATIONS(Camera, Actor)
+};
+
+int main()
+{
+	std::shared_ptr<Actor> player = std::make_shared<Player>();
+	std::shared_ptr<Player> targetPlayer = Cast<Player>(player);
+}
+
+//#define TO_NAME(x) #x
+//#define CLASS_NAME(type)					\
+//	public:									\
+//	virtual const char* ClassName() const	\
+//	{										\
+//		return TO_NAME(type);				\
+//	}										\
+
+//class Actor
+//{
+//	CLASS_NAME(Actor)
+////public: virtual const char* ClassName() const {
+////	return "Actor";
+////};
+//public:
+//	static size_t TypeId()
+//	{
+//		static int runTimeTypeId = 0;
+//		return reinterpret_cast<size_t>(&runTimeTypeId);
+//	}
+//
+//	//virtual const char* ClassName() const 
+//	//{
+//	//	//return "Actor"; 
+//	//	return TO_NAME(Actor);
+//	//}
+//};
+//
+//class Player : public Actor
+//{
+//	CLASS_NAME(Player)
+//public:
+//	static size_t TypeId()
+//	{
+//		static int runTimeTypeId = 0;
+//		return reinterpret_cast<size_t>(&runTimeTypeId);
+//	}
+//
+//private:
+//	//virtual const char* ClassName() const override 
+//	//{
+//	//	return TO_NAME(Player);
+//	//	//return "Player"; 
+//	//}
+//};
+
+//int main()
+//{
+//	Actor* actor = new Actor();
+//	Actor* player = new Player();
+//
+//	std::cout << actor->ClassName() << "\n";
+//	std::cout << player->ClassName() << "\n";
+//
+//	//if (strcmp(actor->ClassName(), player->ClassName()) == 0)
+//	//{
+//	//	std::cout << "같은 타입\n";
+//	//}
+//	//else
+//	//{
+//	//	std::cout << "다른 타입\n";
+//	//}
+//
+//	if (actor->TypeId() == player->TypeId())
+//	{
+//		std::cout << "같은 타입\n";
+//	}
+//	else
+//	{
+//		std::cout << "다른 타입";
+//	}
+//
+//	delete actor;
+//	actor = nullptr;
+//	delete player;
+//	player = nullptr;
+//}
